1514d6e998798f8a0ac7ccb0b21ab3c5eaae4a3f
Tamiriam Fernandes <54744835+tamiriamfernandes@users.noreply.github.com>
2024-11-22 00:01:46 -0300

    aula 2 criado rota de busca por id

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 import express from 'express';
 
+const posts = [
+    {
+        id: 1,
+        descricao: 'Teste Descrição 1',
+        imagem: 'https://placecats.com/millie/300/150'
+    },
+    {
+        id: 2,
+        descricao: 'Teste Descrição 2',
+        imagem: 'https://placecats.com/millie/300/150'
+    },
+    {
+        id: 3,
+        descricao: 'Teste Descrição 3',
+        imagem: 'https://placecats.com/millie/300/150'
+    },
+    {
+        id: 4,
+        descricao: 'Teste Descrição 4',
+        imagem: 'https://placecats.com/millie/300/150'
+    },
+
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Servidor escutando...');
 });
 
-app.get('/api', (req, res) => {
-    res.status(200).send('Imersão Back-End');
+function getById(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+};
+
+app.get('/api/posts/:id', (req, res) => {
+    const index = getById(req.params.id);
+    res.status(200).json(posts[index]);
+});
+
+app.get('/api/posts', (req, res) => {
+    res.status(200).json(posts);
 });
